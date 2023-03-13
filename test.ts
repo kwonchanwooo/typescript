50dfd75955f48158888c699dcd6c4bfc816000ed
kwonchanwooo <rlaalfk121@gmail.com>
2023-03-13 10:34:00 +0900

    first

---
+++ test.ts
@@ -0,0 +1,8 @@
+function getLength<a>(arr: a[]): number {
+	return arr.length;
+}
+
+const arr1 = [1, 2, 3];
+const arr2 = ['a', 'b', 'c'];
+const arr3 = [true, false, true];
+const arr4 = ['string', 2, true];
